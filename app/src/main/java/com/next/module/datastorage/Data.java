79f3db5018a39fcbb667a4048c942c221be4a46e
Afton <next-era@foxmail.com>
2024-05-21 19:49:10 +0800

    修复获取注解失效的问题

--- app/src/main/java/com/next/module/datastorage/Data.java
+++ app/src/main/java/com/next/module/datastorage/Data.java
@@ -1,5 +1,10 @@
 package com.next.module.datastorage;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * ClassName:数据属性注解
  *
@@ -7,6 +12,8 @@ package com.next.module.datastorage;
  * @time 2024/5/21
  * @auditor
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Data {
 
     String key() default "";
